24d0ce25c9614a5e6727881de59ade56f379e07b
victor-slima <victor.slima@rede.ulbra.br>
2025-08-25 21:27:46 -0300

    criando a classe No, e a classe Ldse

---
+++ 4 - Lista Dinamica Simplesmente Encadeada/Ldse.py
@@ -0,0 +1,10 @@
+class No:
+    def __init__(self, valor, proximo):
+        self.info = valor
+        self.prox = proximo
+
+class Ldse:
+    def __init__(self):
+        self.prim = self.ult = None
+        self.quant = 0
+    
---
+++ 4 - Lista Dinamica Simplesmente Encadeada/usaLdse.py
@@ -0,0 +1,3 @@
+import Ldse
+
+l = Ldse.Ldse()
