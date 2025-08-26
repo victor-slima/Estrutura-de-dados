145fcc48c4216692b835b17c850d435e58a8f666
victor-slima <victor.slima@rede.ulbra.br>
2025-08-26 08:46:01 -0300

    subindo atrasado

--- 4 - Lista Dinamica Simplesmente Encadeada/Ldse.py
+++ 4 - Lista Dinamica Simplesmente Encadeada/Ldse.py
@@ -4,7 +4,16 @@ class No:
         self.prox = proximo
 
 class Ldse:
-    def __init__(self):
+    def __init__(self): #  o nao tem tamanho definido
+        # o prim e o ult recebem None
         self.prim = self.ult = None
         self.quant = 0
     
+    def inserir_inicio(self, valor):
+        if self.quant == 0:
+            self.prim = self.ult = No(valor, None)
+
+        else:
+            self.prim = No(valor, self.prim)
+        self.quant += 1
+        
