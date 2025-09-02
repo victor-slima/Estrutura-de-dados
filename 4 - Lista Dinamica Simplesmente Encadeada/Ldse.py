81c06bf2b187b4af3c1afe2dbcc7975a77249469
victor.slima <victorslima.dev@gmail.com>
2025-09-02 16:20:15 -0300

    criando o show e o inserir inicio.

--- 4 - Lista Dinamica Simplesmente Encadeada/Ldse.py
+++ 4 - Lista Dinamica Simplesmente Encadeada/Ldse.py
@@ -2,18 +2,24 @@ class No:
     def __init__(self, valor, proximo):
         self.info = valor
         self.prox = proximo
-
+    
 class Ldse:
-    def __init__(self): #  o nao tem tamanho definido
-        # o prim e o ult recebem None
+    def __init__(self):
         self.prim = self.ult = None
         self.quant = 0
     
+    # Criando o metodo show()
+    def show(self):
+        aux = self.prim
+        while aux != None:
+            print(aux.info, end=" ")
+            aux = aux.prox
+
+
+    # Criando o método inserir_inicio:
     def inserir_inicio(self, valor):
         if self.quant == 0:
             self.prim = self.ult = No(valor, None)
-
         else:
             self.prim = No(valor, self.prim)
         self.quant += 1
-        
