393ebf0d09784bfed9528aaee02f2ba0af0fedd8
victor.slima <victorslima.dev@gmail.com>
2025-11-17 09:05:33 -0300

    palindromo

--- 11 - Pilha/Pilha dinamica/atv.py
+++ 11 - Pilha/Pilha dinamica/atv.py
@@ -1,2 +1,17 @@
 import pilhaD
 
+def ver_palindromo(palavra):
+    palavra = "".join(palavra.split())
+    pilha = pilhaD.PilhaD()
+
+    for char in palavra:
+        pilha.push(char)
+
+    for char in palavra:
+        if pilha.verTopo() != char:
+            return False
+        pilha.pop()
+
+    return True
+
+print(ver_palindromo("socorram me subi no onibus em marrocos"))
