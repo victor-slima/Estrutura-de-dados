57a6be6ea35ba32aaa0e347517692f574e493f69
victor.slima <victorslima.dev@gmail.com>
2025-12-01 18:32:28 -0300

    arvore binaria de busca finalizada

--- 13 - Arvores/Implementacao/Tree.py
+++ 13 - Arvores/Implementacao/Tree.py
@@ -50,6 +50,61 @@ class No:
         print(self.info, end="")
         if self.dir != None:
             self.dir.inOrdem()
+    
+    # PRE ORDEM
+    def preOrdem(self):
+        print(self.info,end=" ")
+        if self.esq != None:
+            self.esq.preOrdem()
+        if self.dir != None:
+            self.dir.preOrdem()
+
+    # POS ORDEM
+    def posOrdem(self):
+        if self.esq != None:
+            self.esq.posOrdem()
+        if self.dir != None:
+            self.dir.posOrdem()
+        print(self.info,end=" ")
+    
+    # qual ordem
+    def qualOrdem(self):
+        if self.dir != None:
+            self.dir.qualOrdem()
+        print(self.info,end=" ")
+        if self.esq != None:
+            self.esq.qualOrdem()
+    
+    # print folhas
+
+    def printFolhas(self):
+        if self.esq != None:
+            self.esq.printFolhas()
+        if self.esq==None and self.dir == None:
+            print(self.info,end=" ")
+        if self.dir != None:
+            self.dir.printFolhas()
+
+    # soma
+    def soma(self):
+        total = self.info
+        if self.esq != None:
+            total += self.esq.soma()
+        if self.dir != None:
+            total += self.dir.soma()
+        return total
+
+    # soma folhas
+    def somaFolhas(self):
+        total = 0
+        if self.esq==None and self.dir == None:
+            total = self.info
+        if self.esq != None:
+            total += self.esq.somaFolhas()
+        if self.dir != None:
+            total += self.dir.somaFolhas()
+        return total
+
 
 class Tree:
     def __init__(self):
@@ -73,4 +128,30 @@ class Tree:
 
     def inOrdem(self):
         if self.raiz != None:
-            self.raiz.inOrdem()   
+            self.raiz.inOrdem()
+
+    def preOrdem(self):
+        if self.raiz != None:
+            self.raiz.preOrdem()
+
+    def posOrdem(self):
+        if self.raiz != None:
+            self.raiz.posOrdem()
+    
+
+    # print folhas
+
+    def printFolhas(self):
+        if self.raiz != None:
+            self.raiz.printFolhas()
+
+    # soma
+    def soma(self):
+        if self.raiz != None:
+            return self.raiz.soma()
+    
+    # soma folhas
+
+    def somaFolhas(self):
+        if self.raiz != None:
+            return self.raiz.somaFolhas()
